f5a951fd9590f7de8d6c37a63bc5faf84798261a
Joe Carter <youlikethaaaat@gmail.com>
2013-08-27 01:08:36 +0100

    a

--- src/main/java/co/d3s/enl/wolfbox/sites/SiteEntry.java
+++ src/main/java/co/d3s/enl/wolfbox/sites/SiteEntry.java
@@ -1,5 +1,7 @@
 package co.d3s.enl.wolfbox.sites;
 
+import java.util.Date;
+
 public abstract class SiteEntry {
 	
 	public SiteEntry()
@@ -10,9 +12,25 @@ public abstract class SiteEntry {
 	abstract public String getAuthor();
 	abstract public String getPreviewURL();
 	abstract public String getImageURL();
-	abstract public String getCreationTime();
+	abstract public Date   getCreationTime();
 	abstract public String getLink();
-	abstract public String getId();
+	abstract public int    getId();
 	abstract public String getTags();
 
+	private String progress;
+	public String getProgress()
+	{
+		return progress;
+	}
+	
+	
+	public void DownloadPreview()
+	{
+		
+	}
+	
+	public void DownloadImage(String filename)
+	{
+		
+	}
 }
--- src/main/java/co/d3s/enl/wolfbox/sites/danbooru/DanbooruEntry.java
+++ src/main/java/co/d3s/enl/wolfbox/sites/danbooru/DanbooruEntry.java
@@ -1,7 +1,51 @@
 package co.d3s.enl.wolfbox.sites.danbooru;
 
+import java.util.Date;
+
 import co.d3s.enl.wolfbox.sites.SiteEntry;
 
 public class DanbooruEntry extends SiteEntry{
+
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPreviewURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getImageURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getCreationTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLink() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getTags() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
