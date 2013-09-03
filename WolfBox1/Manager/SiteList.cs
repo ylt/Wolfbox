05f742ddb5300a6b591219f1ccd0db74a26733b3
Joe Carter <youlikethaaaat@gmail.com>
2013-09-03 21:25:40 +0100

    Some more basic construction

--- WolfBox1/Manager/SiteList.cs
+++ WolfBox1/Manager/SiteList.cs
@@ -9,21 +9,62 @@ namespace WolfBox1.Manager
 {
     abstract class SiteList
     {
+        private XmlNode data;
         public SiteList(XmlNode node) //pass in the List element
         {
+            this.data = node;
         }
+
     }
 
     class SiteListJson : SiteList
     {
-
+        private XmlNode tdata; //type data
+        public SiteListJson(XmlNode node) : base(node)
+        {
+            this.tdata = node.SelectSingleNode("/Json");
+        }
     }
 
     class SiteListXml : SiteList
     {
+
+        private XmlNode tdata; //type data
+        public SiteListXml(XmlNode node) : base(node)
+        {
+            this.tdata = node.SelectSingleNode("/Xml");
+        }
     }
 
     class SiteListHtmlJson : SiteList //for those fucked up booru's
     {
+        public SiteListHtmlJson(XmlNode node) : base(node)
+        {
+        }
     }
+
+    public abstract class SiteEntry
+    {
+        public abstract DateTime CreationTime { get; }
+        public abstract string Author { get; }
+    }
+
+    public class SiteListEntry : SiteEntry
+    {
+    }
+
+    public class SiteListEntryJson
+    {
+    }
+
+    public class SiteListEntryXml
+    {
+    }
+
+    public class SiteListEntryHtmlXml
+    {
+    }
+
+
+    //public class SitePageEntry
 }
