a79c3aec3cfcf46a30289af1690c3c14f0d4e195
Joe Carter <youlikethaaaat@gmail.com>
2013-09-03 03:52:57 +0100

    Half implemented SiteManager, but don't think XML binding is the way to go (removing)

---
+++ WolfBox1/Manager/SiteManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WolfBox1.Manager
+{
+    class SiteManager
+    {
+        public SiteManager()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SitesXML));
+
+            SitesXML sites = (SitesXML)serializer.Deserialize(new StreamReader("sites.xml"));
+
+        }
+
+    }
+
+    [Serializable]
+    [XmlRootAttribute("sites")]
+    private class SitesXML
+    {
+        [XmlElement("site")]
+        public SiteXML[] sites { get; set; }
+    }
+
+    [Serializable()]
+    private class SiteXML
+    {
+        [XmlAttribute("platform")]
+        public string Platform { get; set; }
+
+        [XmlAttribute("domain")]
+        public string Domain { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+    }
+}
--- WolfBox1/Program.cs
+++ WolfBox1/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using WolfBox1.Sites;
 namespace WolfBox1
 {
     static class Program
